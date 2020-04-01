b204fede8691dbcc7a0a9762dc1c06028bc30253
youngwon <youngwon1121@sellmate.co.kr>
2020-04-01 17:58:54 +0900

    Register Instrument app to admin

--- instrument/admin.py
+++ instrument/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Instrument, InstrumentClassification
 
 # Register your models here.
+admin.site.register(InstrumentClassification)
+admin.site.register(Instrument)
